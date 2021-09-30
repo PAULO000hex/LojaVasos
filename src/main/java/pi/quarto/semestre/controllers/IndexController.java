f4b2d502cc77e459e35a1b8fada431cfde990c5a
unknown <wesley132013@gmail.com>
2021-09-29 21:09:38 -0300

    alterações no eccomerce

--- src/main/java/pi/quarto/semestre/controllers/IndexController.java
+++ src/main/java/pi/quarto/semestre/controllers/IndexController.java
@@ -1,10 +1,14 @@
 package pi.quarto.semestre.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import pi.quarto.semestre.models.Produto;
 import pi.quarto.semestre.repositories.ProdutoRepositorio;
 
 @Controller
@@ -17,5 +21,16 @@ public class IndexController {
 	public String produtos(Model model) {
 		model.addAttribute("listaProdutos", produtoRepo.findAll());
 		return "index";
+	}
+	
+	@GetMapping("/produto/detalhes-eccomerce/{id}")
+	public String detalhesProduto(@PathVariable("id") long id, Model model) {
+		Optional<Produto> produto = produtoRepo.findById(id);
+		if (produto.isEmpty()) {
+			throw new IllegalArgumentException("Produto inválido!");
+		}
+		
+		model.addAttribute("produto", produto.get());
+		return "detalhesProduto";
 	}
 }
