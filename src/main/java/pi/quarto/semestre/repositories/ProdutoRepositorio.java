3935ebc3aa2bc6e7fbfd13986861efefb2e2b562
PAULO000hex <69551555+PAULO000hex@users.noreply.github.com>
2021-10-17 21:15:20 -0300

    teste puxando vaoss true

--- src/main/java/pi/quarto/semestre/repositories/ProdutoRepositorio.java
+++ src/main/java/pi/quarto/semestre/repositories/ProdutoRepositorio.java
@@ -1,6 +1,9 @@
 package pi.quarto.semestre.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pi.quarto.semestre.models.Produto;
@@ -8,4 +11,7 @@ import pi.quarto.semestre.models.Produto;
 @Repository
 public interface ProdutoRepositorio extends JpaRepository<Produto, Long> {
 
+	@Query( value = "select * from Produto p where p.status = true", nativeQuery = true)
+	public List<Produto>findByStatus(boolean status);
+	
 }
